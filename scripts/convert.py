e073f9712ab4944b2491614ab8967789e7ce6a0a
Alessandro <carinellialessandro31@gmail.com>
2023-11-02 14:23:33 +0100

    Aggiornamento conversioni automatiche.

--- scripts/convert.py
+++ scripts/convert.py
@@ -2,17 +2,17 @@ import os
 import pdfkit
 
 
-file_list = [('./html/Fisiologia Umana II/MCM.html', 'Fisio2'),
-             ('./html/Fisiologia Umana II/UF.html', 'Fisio2'),
-             ('./html/Fisiologia Umana II/LC.html', 'Fisio2'),
-             ('./html/Fisiopatologia Generale e Patologia Clinica/AC.html', 'Fisiopato'),
-             ('./html/Fisiopatologia Generale e Patologia Clinica/AP.html', 'Fisiopato'),
-             ('./html/Microbiologia/LR.html', 'Microbio'),
-             ('./html/Microbiologia/AL.html', 'Microbio'),
-             ('./html/Microbiologia/MP.html', 'Microbio'),
-             ('./html/Semeiotica e Metodologia Clinica/FF.html', 'Semeiotica'),
-             ('./html/Semeiotica e Metodologia Clinica/GDC.html', 'Semeiotica'),
-             ('./html/Semeiotica e Metodologia Clinica/SC.html', 'Semeiotica')]
+file_list = [('./html/Fisio2-MCM.html', 'Fisio2'),
+             ('./html/Fisio2-UF.html', 'Fisio2'),
+             ('./html/Fisio2-LC.html', 'Fisio2'),
+             ('./html/Fisiopato-AC.html', 'Fisiopato'),
+             ('./html/Fisiopato-AP.html', 'Fisiopato'),
+             ('./html/Microbio-LR.html', 'Microbio'),
+             ('./html/Microbio-AL.html', 'Microbio'),
+             ('./html/Microbio-MP.html', 'Microbio'),
+             ('./html/Semeiotica-FF.html', 'Semeiotica'),
+             ('./html/Semeiotica-GDC.html', 'Semeiotica'),
+             ('./html/Semeiotica-SC.html', 'Semeiotica')]
 
 for file, subject in file_list:
     path = os.path.abspath(file)
