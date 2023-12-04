976b1762a69bf23ae6d9bb3e35c98ddb2e8996a5
Alessandro <carinellialessandro31@gmail.com>
2023-12-04 09:16:09 +0100

    Updated Actions

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,6 +7,7 @@ file_list = [('./html/Fisio2-MCM.html', 'Fisio2'),
              ('./html/Fisio2-LC.html', 'Fisio2'),
              ('./html/Fisiopato-AC.html', 'Fisiopato'),
              ('./html/Fisiopato-AP.html', 'Fisiopato'),
+             ('./html/Fisiopato-LC.html', 'Fisiopato'),
              ('./html/Microbio-LR.html', 'Microbio'),
              ('./html/Microbio-AL.html', 'Microbio'),
              ('./html/Microbio-MP.html', 'Microbio'),
@@ -18,4 +19,4 @@ for file, subject in file_list:
     path = os.path.abspath(file)
     with open(path, 'r') as file:
         code = file.read()
-        pdfkit.from_string(code, f'pdf/{subject}-{str(file).split("/")[-1].split(".")[0]}.pdf')
+        pdfkit.from_string(code, f'pdf/{str(file).split("/")[-1].split(".")[0]}.pdf')
